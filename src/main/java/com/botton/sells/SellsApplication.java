ff1c13c925142c195f070daaad120dbdbb8abc57
= <=>
2020-09-20 23:52:59 -0300

    Initial

---
+++ src/main/java/com/botton/sells/SellsApplication.java
@@ -0,0 +1,13 @@
+package com.botton.sells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botton/sells/SellsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botton.sells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
