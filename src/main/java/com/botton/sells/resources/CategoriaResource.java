0d6d2c31390feb812480059d65c8f6f4afe8e632
luizbotton <31282962+luizbotton@users.noreply.github.com>
2020-09-21 17:47:51 -0300

    categoria resource e entidade adicionadas

--- src/main/java/com/botton/sells/resources/CategoriaResource.java
+++ src/main/java/com/botton/sells/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.botton.sells.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.botton.sells.domain.Categoria;
 
 @RestController
 //nome do conceito no plural
@@ -11,8 +16,16 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST Working";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Informatica");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 }
